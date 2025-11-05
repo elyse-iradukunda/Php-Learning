c04d2a8602d447de350ca241496e0d0538583b8d
Irael <ellycreativity8@gmail.com>
2025-11-05 22:45:12 +0200

    chores: today findings

---
+++ today.php
@@ -0,0 +1,19 @@
+<?php
+
+// $fruits = array("apple","banana","Orange");
+$fruits = ["apple","banana","Orange"];
+$cars = ["toyota","benze","BMW"];
+
+$merged =array_merge($fruits,$cars);
+$
+
+ print_r($merged);
+ 
+ echo $merged;
+
+ foreach( $fruits as $i  ){
+     echo $i." hello";
+     echo "\n";
+ }
+
+?>
