9bca3eaa6f051057178d2325211c8ececc9f8984
Irael <ellycreativity8@gmail.com>
2025-11-05 23:58:39 +0200

    chore about array

--- today.php
+++ today.php
@@ -31,5 +31,19 @@ foreach($cars as $i => $v){
  echo "\n";
 }
 
+<?php
 
+// $fruits = array("apple","banana","Orange");
+$colors = ["orange","green","Blue",'white','black','purple','yellow'];
+
+$num =[2,4,6];
+
+array_push($num,8,10); // adding the element at the end.
+array_unshift($num,2,0); // adding element ad the beginning.
+array_shift($num); // removing element at the beginning.
+array_pop($num); // remove element at the end
+unset($num[2]); // removing element in the middle anyhere you want.
+print_r($num);
+
+?>
 ?>
