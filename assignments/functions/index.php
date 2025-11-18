c7c74bd86a62c4ff6d519395436896407d89371f
Irael <ellycreativity8@gmail.com>
2025-11-18 16:44:51 +0200

    here is the things we leant about functions in php

---
+++ assignments/functions/index.php
@@ -0,0 +1,85 @@
+
+
+<?php
+ function ceo($string){
+ $trim = trim($string);
+ $replace = str_replace(" ","_",$trim);
+ $ucword = ucwords($replace);
+  $upper = strtoupper($ucword);
+  return $ucword;
+}
+
+echo(ceo("Auto-formatting title in a CMS or blog system")); 
+echo "<br>";
+?>
+
+
+<?php
+function hashProduct($p){
+
+    return md5($p);
+
+}
+echo hashProduct(1234);
+echo "<br>";
+?>
+
+
+<?php
+function Charcode(){
+    $code = '';
+    for( $i = 0; $i<8; $i++){
+       
+        $code .=chr(rand(65,90));
+    }
+
+ return $code;
+}
+
+echo(Charcode());
+
+?>
+
+
+<?php
+
+function Convert($c,$f){
+    
+    $findCeliciusToFa = ($c* 9/5)+32;
+    $findFaToCelicus =($f-32)* 5/9;
+
+    return "The Fahrenheit".$findCeliciusToFa ."and ".$findFaToCelicus;
+}
+
+echo(Convert(13,50));
+?>
+
+<?php 
+ 
+ function length($str){
+    if(strlen(($str)==5)&& (ctype_alnum($str)==true)){
+        
+        return "this is valid format";
+    }
+    else{
+        return "watokombey";
+    }
+ }
+echo (length("fb355"));
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
